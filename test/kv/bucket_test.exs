7230804cc215630ea9d71c4ec97dfc4db491b51e
Mike Green <mike.green@simplethread.com>
2019-01-29 13:37:03 -0500

    Create a bucket module backed by Agents

---
+++ test/kv/bucket_test.exs
@@ -0,0 +1,23 @@
+defmodule KV.BucketTest do
+  use ExUnit.Case, async: true
+
+  setup do
+    {:ok, bucket} = KV.Bucket.start_link([])
+    %{bucket: bucket}
+  end
+
+  test "stores values by key", %{bucket: bucket} do
+    assert KV.Bucket.get(bucket, "milk") == nil
+
+    KV.Bucket.put(bucket, "milk", 3)
+    assert KV.Bucket.get(bucket, "milk") == 3
+  end
+
+  test "deletes values by key", %{bucket: bucket} do
+    KV.Bucket.put(bucket, "eggs", 12)
+    assert KV.Bucket.get(bucket, "eggs") == 12
+
+    KV.Bucket.delete(bucket, "eggs")
+    assert KV.Bucket.get(bucket, "eggs") == nil
+  end
+end
